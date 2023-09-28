722847133e59d81c4ace982343ec89ab44878247
keerthi <146065976+keeraj-2519@users.noreply.github.com>
2023-09-29 02:01:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var stringName="Dell";
+var idNumber=12369;
+var jrBoolean=true;
+var expNUll="Null";
+
+function string(){
+    document.getElementById("l1").textContent=stringName;
+}
+function number(){
+    document.getElementById("l2").textContent=idNumber;
+    
+}
+function boolean(){
+    document.getElementById("l3").textContent=jrBoolean;
+}
+function nullData(){
+    document.getElementById("l4").textContent=expNUll;
+}
